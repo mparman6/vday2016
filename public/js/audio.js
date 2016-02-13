054c87159890c84cdc480d947498fcc5698e618e
Matt <mparman6@yahoo.com>
2016-02-13 12:35:07 -0500

    added working music and styles

---
+++ public/js/audio.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', '/aud/John_Legend_-_All_of_Me.mp3');
+        audioElement.setAttribute('autoplay', 'autoplay');
+        //audioElement.load()
+
+        $.get();
+
+        audioElement.addEventListener("load", function() {
+            audioElement.play();
+        }, true);
+
+        $('#play').click(function() {
+            audioElement.play();
+        });
+
+        $('#pause').click(function() {
+            audioElement.pause();
+        });
+    });
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var PORT = process.env.PORT || 8090;
 app.use("/js", express.static("public/js"));
 app.use("/css", express.static("public/css"));
 app.use("/img", express.static("public/img"));
+app.use("/font", express.static("public/font"));
+app.use("/aud", express.static("public/audio"));
 
 app.get("/", function(req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
